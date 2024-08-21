a6a081df904a030f4fa774112750b94fa06bcc77
Shant Dashjian <shantgdashjian@gmail.com>
2024-08-21 14:55:21 +0400

    Add AI-specific code to talk to Claude

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import Anthropic from '@anthropic-ai/sdk'
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+}
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const anthropic = new Anthropic({
+			apiKey: env.ANTHROPIC_API_KEY,
+		})
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders })
+		}
+
+		try {
+			const messages = await request.json()
+			const msg = await anthropic.messages.create({
+				model: 'claude-3-5-sonnet-20240620',
+				max_tokens: 1024,
+				system: 'You are a text summarizer. When asked to summarize a text, send back the summary of it. Please only send back the summary without prefixing it with things like "Summary" or telling where the text is from. Also give me the summary as if the original author wrote it and without using a third person voice.',
+				messages: messages
+			})
+			return new Response(JSON.stringify(msg.content[0].text), { headers: corsHeaders })
+		} catch (error) {
+			return new Response(JSON.stringify({ error: error }), { status: 500, headers: corsHeaders })
+		}
 	},
-};
+}
